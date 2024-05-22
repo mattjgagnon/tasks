249151e41859ba57e84758aad4e5424605927a76
Matthew Gagnon <MatthewGagnon.Software@gmail.com>
2024-05-22 12:48:31 -0400

    Add tasks migration

---
+++ Modules/Tasks/Database/Migrations/2024_05_18_000003_create_tasks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+final class CreateTasksTable extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->string('title');
+            $table->text('description');
+            $table->string('status');
+            $table->string('priority');
+            $table->unsignedBigInteger('assigned_user_id')->nullable();
+            $table->unsignedBigInteger('reporter_user_id');
+            $table->date('due_date')->nullable();
+            $table->timestamps();
+
+            $table->foreign('assigned_user_id')->references('id')->on('users')->onDelete('set null');
+            $table->foreign('reporter_user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
