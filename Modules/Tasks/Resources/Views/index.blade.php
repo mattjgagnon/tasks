a09994878fedcdb59fb661de596f9b6307542051
Matthew Gagnon <MatthewGagnon.Software@gmail.com>
2024-05-22 12:45:16 -0400

    Add Tasks view

---
+++ Modules/Tasks/Resources/Views/index.blade.php
@@ -0,0 +1,12 @@
+<?php
+
+@extends('layouts.app')
+
+@section('content')
+    <h1>Tasks</h1>
+    <ul>
+        @foreach ($tasks as $task)
+            <li>{{ $task->title }}</li>
+        @endforeach
+    </ul>
+@endsection
